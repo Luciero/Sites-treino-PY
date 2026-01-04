a491ea1348fba0d1051ead5ee98a5b9156d3c6ce
Luciero <pedrohs@ufba.br>
2026-01-04 12:45:49 -0300

    Adicionado funcao de excluir o post

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, abort
 from comunidadeimpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil, FormCriarPost
 from comunidadeimpressionadora import app, bcrypt, db
 from comunidadeimpressionadora.models import Usuario, Post
@@ -147,3 +147,17 @@ def exibir_post(post_id):
     else:
         form = None
     return render_template('post.html', post=post, form=form)
+
+
+@app.route('/post/<post_id>/excluir', methods=['GET', 'POST'])
+@login_required
+def excluir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post Excluido com Sucesso!', 'alert-danger')
+        return redirect(url_for('home'))
+    else:
+        abort(403)
+
